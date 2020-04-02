0becd8ea0a70b6f6fd9fa555befb72255d62f49f
Rob Stradling <rob@sectigo.com>
2020-04-02 22:51:12 +0100

    Run test_websites_monitor after update_ccadb_certificates.sh (to avoid a deadlock), rather than adding it to crontab.

--- ccadb/update_ccadb_certificates.sh
+++ ccadb/update_ccadb_certificates.sh
@@ -8,3 +8,7 @@ if [ -s ccadb_all_certificate_records.csv.new ]; then
 else
   echo "Failed to download https://ccadb-public.secure.force.com/ccadb/AllCertificateRecordsCSVFormat"
 fi
+
+# Check the Test Websites and update the monitoring page.
+cd ~/certwatch/test_websites_monitor/bin
+./test_websites_monitor
