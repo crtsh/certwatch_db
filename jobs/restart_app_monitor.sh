59973d3be4e15a31e42ac09fea212cc729f25fd1
Rob Stradling <rob@sectigo.com>
2020-04-03 17:59:05 +0100

    Hopefully fix race condition in restart_app_monitor.sh.

--- jobs/restart_app_monitor.sh
+++ jobs/restart_app_monitor.sh
@@ -9,11 +9,16 @@ while [ $result -eq 0 ]; do
   fi
 done
 
-screen -S `basename "$1"` -Q select . >/dev/null
-result=$?
-if [ "${result}" -ne "0" ] ; then
-  cd `dirname "$1"`
-  screen -d -m -S `basename "$1"` "$1" 2>/dev/null
-else
-  echo "$1: screen -Q did not work as expected"
-fi
+result=0
+for i in 1 2 3 4 5; do
+  screen -S `basename "$1"` -Q select . >/dev/null
+  result=$?
+  if [ $result -ne 0 ] ; then
+    cd `dirname "$1"`
+    screen -d -m -S `basename "$1"` "$1" 2>/dev/null
+    break
+  elif [ $i -gt 4 ] ; then
+    echo "$1: screen -Q did not work as expected"
+  fi
+  sleep 1
+done
