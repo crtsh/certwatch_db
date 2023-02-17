2feb9cd61e1a748fa91d8edf15b51c982704de2d
Rob Stradling <rob@sectigo.com>
2023-02-17 18:04:52 +0000

    Hopefully make downloading CCADB public reports more resilient to failure.

--- ccadb/update_caowner_information.sh
+++ ccadb/update_caowner_information.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget -O ccadb_caowner_information.csv https://ccadb-public.my.salesforce-sites.com/mozilla/CAInformationReportCSVFormat
-if [ -s ccadb_caowner_information.csv ]; then
+wget -O ccadb_caowner_information.csv.new https://ccadb-public.my.salesforce-sites.com/mozilla/CAInformationReportCSVFormat
+RESULT=$?
+if [ "$RESULT" -eq "0" ]; then
+  mv ccadb_caowner_information.csv.new ccadb_caowner_information.csv
   psql -f update_caowner_information.sql -h $PGHOST -d certwatch -U certwatch
 else
   echo "Failed to download https://ccadb-public.my.salesforce-sites.com/mozilla/CAInformationReportCSVFormat"
--- ccadb/update_ccadb_certificates.sh
+++ ccadb/update_ccadb_certificates.sh
@@ -2,8 +2,9 @@
 
 # Process CCADB certificates.
 wget -O ccadb_all_certificate_records.csv.new https://ccadb-public.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormat
-if [ -s ccadb_all_certificate_records.csv.new ]; then
-  cp ccadb_all_certificate_records.csv.new ccadb_all_certificate_records.csv
+RESULT=$?
+if [ "$RESULT" -eq "0" ]; then
+  mv ccadb_all_certificate_records.csv.new ccadb_all_certificate_records.csv
   psql -f update_ccadb_certificates.sql -h $PGHOST -d certwatch -U certwatch
 else
   echo "Failed to download https://ccadb-public.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormat"
