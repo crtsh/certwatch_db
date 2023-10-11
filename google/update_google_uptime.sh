0d98c5554760626ff8f89fdab01840114b49a5b5
Rob Stradling <rob@sectigo.com>
2023-10-11 16:02:42 +0100

    Change the Google log uptime monitoring CSV URL.

--- google/update_google_uptime.sh
+++ google/update_google_uptime.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.profile
-wget -O ~/certwatch/google/google_uptime.csv https://www.gstatic.com/ct/compliance/uptime.csv
+wget -O ~/certwatch/google/google_uptime.csv https://www.gstatic.com/ct/compliance/min_uptime.csv
 psql -f ~/certwatch/google/update_google_uptime.sql -h $PGHOST -d certwatch -U certwatch
