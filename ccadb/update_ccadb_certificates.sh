217091ad6ddb9819cd99f46d301bed8a105e0325
Rob Stradling <rob@sectigo.com>
2024-01-12 16:36:21 +0000

    update_ccadb_certificates: Switch to the new 'All Cert Information v2' CSV report from CCADB.

--- ccadb/update_ccadb_certificates.sh
+++ ccadb/update_ccadb_certificates.sh
@@ -2,14 +2,14 @@
 source ~/.profile
 
 # Process CCADB certificates.
-wget -O ccadb_all_certificate_records.csv.new https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormat
+wget -O ccadb_all_certificate_records.csv.new https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv2
 RESULT=$?
 if [ "$RESULT" -eq "0" ]; then
   mv ccadb_all_certificate_records.csv.new ccadb_all_certificate_records.csv
   sed -i "s/,$//g" ccadb_all_certificate_records.csv
   psql -f update_ccadb_certificates.sql -h $PGHOST -d certwatch -U certwatch
 else
-  echo "Failed to download https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormat"
+  echo "Failed to download https://ccadb.my.salesforce-sites.com/ccadb/AllCertificateRecordsCSVFormatv2"
 fi
 
 # Check the Test Websites and update the monitoring page.
