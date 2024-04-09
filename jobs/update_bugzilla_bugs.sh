13bbfc6f0b98297460cdaa722e3deb7cbc832a70
Rob Stradling <rob@sectigo.com>
2024-04-09 15:46:18 +0100

    Use currently relevant Bugzilla program and component names.

--- jobs/update_bugzilla_bugs.sh
+++ jobs/update_bugzilla_bugs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.profile
-wget -O bugzilla_bugs.json "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,whiteboard,component,status,resolution,creation_time,last_change_time&component=CA%20Certificate%20Compliance&component=CA%20Certificate%20Root%20Program&limit=0&product=NSS"
+wget -O bugzilla_bugs.json "https://bugzilla.mozilla.org/rest/bug?include_fields=id,summary,whiteboard,component,status,resolution,creation_time,last_change_time&product=NSS&component=CA%20Certificates%20Code&component=Libraries&product=CA%20Program&component=CA%20Certificate%20Compliance&component=CA%20Certificate%20Root%20Program&limit=0"
 psql -f ~/certwatch/sql/update_bugzilla_bugs.sql -h $PGHOST -d certwatch -U certwatch
